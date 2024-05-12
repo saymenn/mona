7fcb8db77260df5c4e849f373ad787342a6625f1
saymenn <147929584+saymenn@users.noreply.github.com>
2024-05-12 05:02:11 +0100

    Create mona.py

---
+++ mona.py
@@ -0,0 +1,49 @@
+import os
+import sys
+import time
+
+base_path = os.getcwd()
+
+
+def new_sub(domain):
+    try:
+        new_sub_path = base_path + "/" + domain
+        os.mkdir(new_sub_path)
+        os.system(f"subfinder -d {domain} -all -o {new_sub_path}/{domain}")
+    except Exception as err:
+        print("[-] ERROR at new_sub func", err)
+        sys.exit(0)
+
+def monitor(domain):
+    domain_path = base_path + "/" + domain + "/" + domain
+    if os.path.exists(domain_path):
+        try:
+            os.system(f"subfinder -d {domain} -all | anew {domain_path} | notify -pc ~/.config/notify/provider-config.yaml -bulk")
+        except Exception as err:
+            print("[-] ERROR at the monitor func ", err)
+            sys.exit(0)
+    else:
+        print("[-] FATAL monitor trying to load a non existent path")
+        sys.exit(0)
+
+
+def main():
+    root_domains = base_path + "/" + "root_domains.txt"
+    if os.path.exists(root_domains):
+        with open(root_domains, 'r') as roots_file:
+            for root in roots_file:
+                root = root.strip()
+                root_path = base_path + "/" + root
+                if not os.path.exists(root_path):
+                    new_sub(root)
+                else:
+                    monitor(root)
+    else:
+        print("[-] No root_domains.txt file exiting ...")
+        sys.exit(0)
+
+
+while True:
+    main()
+    print("[+] ==> The script is currently done until the next attempt ...")
+    time.sleep(21600)
